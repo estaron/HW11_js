f86fe271d42e7be5ab8a7e90655ad7d43034eefb
estaron <42524975+estaron@users.noreply.github.com>
2019-02-20 20:43:29 -0600

    Here is my submisssion

---
+++ Solution/static/js/app.js
@@ -0,0 +1,46 @@
+// from data.js
+var tableData = data;
+
+
+// where we will insert the table
+var tbody = d3.select("tbody");
+
+//this reads every report
+//tableData.forEach(report => console.log(report))
+
+
+// YOUR CODE HERE!
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();  
+  
+  // Select the input element and get the raw value
+  var inputdate = d3.select("#datetime");
+
+  var inputValue = inputdate.property("value");  
+
+  console.log(inputValue)
+   
+  var filteredData = tableData.filter(report => report.datetime === inputValue);
+
+  console.log(filteredData);
+
+  tableData.forEach((event) => {
+    var emptyrow = d3.selectAll('tbody>tr');
+    Object.entries(event).forEach(() => {
+      var emptycell = d3.selectAll('tbody>td').text('');
+    });
+  });
+
+  filteredData.forEach(function(report) {
+    var row = tbody.append("tr");
+    Object.entries(report).forEach(function([key, value]) {
+        var cell = tbody.append("td");
+        cell.text(value);
+      });
+    })
+})
